2257a42bad836ebfa1c134790ac04e90543e0e81
SeungGyu <phillipskl123@gmail.com>
2025-03-29 01:29:42 +0900

    [add] 개발 환경 setting

---
+++ src/main/java/com/mjmeal/mj_cafeteria_team_feedback_be/MjCafeteriaTeamFeedbackBeApplication.java
@@ -0,0 +1,13 @@
+package com.mjmeal.mj_cafeteria_team_feedback_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MjCafeteriaTeamFeedbackBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MjCafeteriaTeamFeedbackBeApplication.class, args);
+	}
+
+}
